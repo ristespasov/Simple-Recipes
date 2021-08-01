0b021131cb88e84ba1de2fa0747df87eebc53b12
ristespasov <ristespasov@hotmail.com>
2021-08-01 21:03:03 +0200

    contentful api configured

--- src/pages/recipes.js
+++ src/pages/recipes.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import Layout from '../components/Layout'
+import React from "react"
+import Layout from "../components/Layout"
 
 const recipes = () => {
-    return (
-        <Layout>
-            Recipes page
-        </Layout>
-    )
+  return (
+    <Layout>
+      <div>RECIPES PAGE</div>
+    </Layout>
+  )
 }
 
 export default recipes
