4100088a2afcefd7adfe9892f0baad6daae86714
ristespasov <ristespasov@hotmail.com>
2021-08-07 16:07:10 +0200

    about page set

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,10 +2,14 @@ import React from "react"
 import Layout from "../components/Layout"
 import { StaticImage } from "gatsby-plugin-image"
 import { Link, graphql } from "gatsby"
-// import RecipesList from "../components/RecipesList"
+import RecipesList from "../components/RecipesList"
 // import SEO from "../components/SEO"
 
-const About = () => {
+const About = ({
+  data: {
+    allContentfulRecipe: { nodes: recipes },
+  },
+}) => {
   return (
     <Layout>
       {/* <SEO title="About" /> */}
@@ -34,30 +38,30 @@ const About = () => {
         </section>
         <section className="featured-recipes">
           <h5>Look at this Awesomesouce!</h5>
-          {/* <RecipesList recipes={recipes} /> */}
+          <RecipesList recipes={recipes} />
         </section>
       </main>
     </Layout>
   )
 }
 
-// export const query = graphql`
-//   {
-//     allContentfulRecipe(
-//       sort: { fields: title, order: ASC }
-//       filter: { featured: { eq: true } }
-//     ) {
-//       nodes {
-//         id
-//         title
-//         cookTime
-//         prepTime
-//         image {
-//           gatsbyImageData(layout: CONSTRAINED, placeholder: BLURRED)
-//         }
-//       }
-//     }
-//   }
-// `
+export const query = graphql`
+  {
+    allContentfulRecipe(
+      sort: { fields: title, order: ASC }
+      filter: { featured: { eq: true } }
+    ) {
+      nodes {
+        id
+        title
+        cookTime
+        prepTime
+        image {
+          gatsbyImageData(layout: CONSTRAINED, placeholder: BLURRED)
+        }
+      }
+    }
+  }
+`
 
 export default About
